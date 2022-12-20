903fdfdde28c29440cb667355a28fc260dab9eb2
nkrit1 <80827211+nkrit1@users.noreply.github.com>
2022-12-20 18:24:00 +0300

    Спринт 2. ДЗ 1. Задание 2

---
+++ largest_number.py
@@ -0,0 +1,13 @@
+import functools
+
+
+def mycmp(a, b):
+    return -1 if (a[0] * 10 ** len(b[1]) + b[0]) > (b[0] * 10 ** len(a[1]) + a[0]) else 1
+
+
+n = int(input())
+if n != 0:
+    array = [tuple([int(x), x]) for x in input().split(' ')]
+    array.sort(key=functools.cmp_to_key(mycmp))
+    print(''.join([x[1] for x in array]), end='')
+
