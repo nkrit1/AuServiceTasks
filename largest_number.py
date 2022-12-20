8178bd68fddcc980bf31f37f5437faafa94cd896
nkrit1 <80827211+nkrit1@users.noreply.github.com>
2022-12-20 19:01:57 +0300

    Проверки на целое и неотрицательное число

--- largest_number.py
+++ largest_number.py
@@ -5,9 +5,14 @@ def mycmp(a, b):
     return -1 if (a[0] * 10 ** len(b[1]) + b[0]) > (b[0] * 10 ** len(a[1]) + a[0]) else 1
 
 
+flag = True
 n = int(input())
 if n != 0:
-    array = [tuple([int(x), x]) for x in input().split(' ')]
-    array.sort(key=functools.cmp_to_key(mycmp))
-    print(''.join([x[1] for x in array]), end='')
+    array = [[int(float(x)), str(int(float(x)))] for x in input().split(' ')]
+    for el in array:
+        if el[0] < 0:
+            flag = False
+    if flag:
+        array.sort(key=functools.cmp_to_key(mycmp))
+        print(''.join([x[1] for x in array]), end='')
 
